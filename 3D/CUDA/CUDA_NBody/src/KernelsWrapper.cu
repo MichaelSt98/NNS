390a346180ef032fab52856cfddbb89ffddf4b63
Michael Staneker <50531288+MichaelSt98@users.noreply.github.com>
2021-02-23 17:00:33 +0100

    Fixed std::cout of CUDA variables

--- 3D/CUDA/CUDA_NBody/src/KernelsWrapper.cu
+++ 3D/CUDA/CUDA_NBody/src/KernelsWrapper.cu
@@ -7,11 +7,12 @@
 float kernel::setDrawArray(float *ptr, float *x, float *y, float *z, int n) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "setDrawArrayKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "setDrawArrayKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     setDrawArrayKernel<<< gridSize, blockSize>>>(ptr, x, y, z, n);
 
     cudaEventRecord(stop,0);
@@ -27,11 +28,12 @@ float kernel::resetArrays(int *mutex, float *x, float *y, float *z, float *mass,
                           float *minZ, float *maxZ, float *top, int n, int m) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "resetArraysKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "resetArraysKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     resetArraysKernel<<< gridSize, blockSize >>>(mutex, x, y, z, mass, count, start, sorted, child, index,
             maxX, minY, maxY, minZ, maxZ, n, m);
 
@@ -48,11 +50,12 @@ float kernel::computeBoundingBox(int *mutex, float *x, float *y, float *z,
                                  float *maxX, float *minY, float *maxY, float *minZ, float *maxZ, float *top, int n) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "computeBoundingBoxKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "computeBoundingBoxKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     computeBoundingBoxKernel<<< gridSize, blockSize >>>(mutex, x, y, z, maxX, minY, maxY, minZ, maxZ, n);
 
     cudaEventRecord(stop,0);
@@ -69,11 +72,12 @@ float kernel::buildTree(float *x, float *y, float *z, float *mass, int *count, i
                         float *minZ, float *maxZ, int n, int m) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "buildTreeKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "buildTreeKernel<<< " /*<< gridSize << ", " << blockSize */ << " >>>" << std::endl;
     buildTreeKernel<<< gridSize, blockSize >>>(x, y, z, mass, count, start, child, index,
             minX, maxX, minY, maxY, minZ, maxZ, n, m);
 
@@ -89,11 +93,12 @@ float kernel::buildTree(float *x, float *y, float *z, float *mass, int *count, i
 float kernel::centreOfMass(float *x, float *y, float *z, float *mass, int *index, int n) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "centreOfMassKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "centreOfMassKernel<<< " /*<< gridSize << ", " << blockSize */ << " >>>" << std::endl;
     centreOfMassKernel<<< gridSize, blockSize >>>(x, y, z, mass, index, n);
 
     cudaEventRecord(stop,0);
@@ -108,11 +113,12 @@ float kernel::centreOfMass(float *x, float *y, float *z, float *mass, int *index
 float kernel::sort(int *count, int *start, int *sorted, int *child, int *index, int n) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "sortKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "sortKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     sortKernel<<< gridSize, blockSize>>>(count, start, sorted, child, index, n);
 
     cudaEventRecord(stop,0);
@@ -129,11 +135,12 @@ float kernel::computeForces(float *x, float *y, float *z, float *vx, float *vy, 
                             float *minX, float *maxX, int n, float g) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "computeForcesKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "computeForcesKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     computeForcesKernel<<<gridSize, blockSize>>>(x, y, z, vx, vy, vz, ax, ay, az,
             mass, sorted, child, minX, maxX, n, g);
 
@@ -146,15 +153,16 @@ float kernel::computeForces(float *x, float *y, float *z, float *vx, float *vy, 
 
 }
 
-void kernel::update(float *x, float *y, float *z, float *vx, float *vy, float *vz,
+float kernel::update(float *x, float *y, float *z, float *vx, float *vy, float *vz,
                     float *ax, float *ay, float *az, int n, float dt, float d) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "updateKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "updateKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     updateKernel<<< gridSize, blockSize >>>(x, y, z, vx, vy, vz, ax, ay, az, n, dt, d);
 
     cudaEventRecord(stop,0);
@@ -166,14 +174,15 @@ void kernel::update(float *x, float *y, float *z, float *vx, float *vy, float *v
 
 }
 
-void kernel::copy(float *x, float *y, float *z, float *out, int n) {
+float kernel::copy(float *x, float *y, float *z, float *out, int n) {
 
     float elapsedTime;
+    cudaEvent_t start, stop; // used for timing
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    std::cout << "copyKernel<<< " << gridSize << ", " << blockSize << " >>>" << std::endl;
+    std::cout << "copyKernel<<< " /*<< gridSize << ", " << blockSize*/ << " >>>" << std::endl;
     copyKernel<<< gridSize, blockSize >>>(x, y, z, out, n);
 
     cudaEventRecord(stop,0);
