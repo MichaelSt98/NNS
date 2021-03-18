5c033aa100fc881c5c72601a4b0b943dc66e093b
Michael Staneker <50531288+MichaelSt98@users.noreply.github.com>
2021-03-18 21:21:58 +0100

    Moved functionality in functions

--- MolecularDynamics/BarnesHutParallel/TestingMPI/src/main.cpp
+++ MolecularDynamics/BarnesHutParallel/TestingMPI/src/main.cpp
@@ -8,23 +8,15 @@
 
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int outputRank = 1;
 
-    MPI_Init(&argc, &argv);
+void initializeParticles(Particle *particle, int N);
+void createLocalParticleList(SubDomainKeyTree s, ParticleList *pList, Particle *particles, int numberOfParticles);
 
-    std::cout << "Starting..." << std::endl;
-    int outputRank = 1;
+void createParticleDatatype(MPI_Datatype datatype);
 
-    SubDomainKeyTree  s;
-    MPI_Comm_rank(MPI_COMM_WORLD, &s.myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &s.numprocs);
-    //s.range = 0; //TODO: set range for sub domain key tree
-
-    if (s.myrank == outputRank) {
-        std::cout << "Creating MPI Datatype for Particle struct..." << std::endl;
-    }
+void createParticleDatatype(MPI_Datatype *datatype) {
     //create MPI datatype for Particle struct
-    MPI_Datatype mpiParticle;
     int mpiParticleLengths[6] = {1, DIM, DIM, DIM, 1, 1};
     // not properly working
     //const MPI_Aint mpiParticleDisplacements[6] ={ 0, sizeof(float), 2*sizeof(float), 3*sizeof(float), 4*sizeof(float), 4*sizeof(float) + sizeof(bool) };
@@ -32,16 +24,12 @@ int main(int argc, char *argv[]) {
     const MPI_Aint mpiParticleDisplacements[6] ={ offsetof(Particle, m), offsetof(Particle, x), offsetof(Particle, v),
                                                   offsetof(Particle, F), offsetof(Particle, moved), offsetof(Particle, todelete) };
     MPI_Datatype mpiParticleTypes[6] = { MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_CXX_BOOL, MPI_CXX_BOOL }; // MPI_C_BOOL ?
-    MPI_Type_create_struct(6, mpiParticleLengths, mpiParticleDisplacements, mpiParticleTypes, &mpiParticle);
-    MPI_Type_commit(&mpiParticle);
+    MPI_Type_create_struct(6, mpiParticleLengths, mpiParticleDisplacements, mpiParticleTypes, datatype);
+    MPI_Type_commit(datatype);
+}
 
-    if (s.myrank == outputRank) {
-        std::cout << "Creating particles for testing..." << std::endl;
-    }
-    //create particles for testing
-    int numberOfParticles = 10;
-    Particle particles[numberOfParticles];
-    for (int i=0; i<numberOfParticles; i++) {
+void initializeParticles(Particle *particles, int N) {
+    for (int i=0; i<N; i++) {
         particles[i].x[0] = (float)i;
         particles[i].x[1] = (float)i;
         particles[i].x[2] = (float)i;
@@ -55,41 +43,26 @@ int main(int argc, char *argv[]) {
         particles[i].todelete = false;
         particles[i].moved = false;
     }
+}
 
-    if (s.myrank == outputRank) {
-        std::cout << "Converting particle array to particle list..." << std::endl;
-    }
-    //convert array to particle list
-    ParticleList *particleList;
+void createLocalParticleList(SubDomainKeyTree s, ParticleList *pList, Particle *particles, int numberOfParticles) {
+
     int particlesPerProcess = numberOfParticles/s.numprocs;
     if (s.myrank == outputRank) {
         std::cout << "particles per process: " << particlesPerProcess << std::endl;
     }
-    auto pList = new ParticleList;
-    particleList = pList;
 
     for (int i=0; i<particlesPerProcess; i++) {
         if (s.myrank == outputRank) {
-            std::cout << "i: " << i << std::endl;
+            std::cout << "create local particle list i: " << i << std::endl;
         }
         pList->p = particles[i + s.myrank*particlesPerProcess];
         pList->next = new ParticleList;
         pList = pList->next;
     }
+}
 
-    /*int counter = 0;
-    while (pList->next && counter < 50) {
-        std::cout << "counter: " << counter << std::endl;
-        pList = pList->next;
-        counter++;
-    }*/
-
-    if (s.myrank == outputRank) {
-        std::cout << "Converting back to an array..." << std::endl;
-    }
-    //convert back to array
-    Particle pArray[particlesPerProcess];
-    Particle pReceived[particlesPerProcess];
+void convertListToArray(SubDomainKeyTree s, ParticleList *particleList, Particle *pArray, int particlesPerProcess) {
     int counter = 0;
     while(particleList->next) {
         if (s.myrank == outputRank) {
@@ -106,7 +79,62 @@ int main(int argc, char *argv[]) {
             std::cout << "\tparticle " << i << ": " << "(" << pArray[i].x[0] << ", " << pArray[i].x[1] << ", " << pArray[i].x[2] << ")" << std::endl;
         }
     }
+}
 
+int main(int argc, char *argv[]) {
+
+    MPI_Init(&argc, &argv);
+
+    /** -------------------------------------- */
+    std::cout << "Starting..." << std::endl;
+
+    SubDomainKeyTree  s;
+    MPI_Comm_rank(MPI_COMM_WORLD, &s.myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &s.numprocs);
+    //s.range = 0; //TODO: set range for sub domain key tree
+
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Creating MPI Datatype for Particle struct..." << std::endl;
+    }
+    //create MPI datatype for Particle struct
+    MPI_Datatype mpiParticle;
+    createParticleDatatype(&mpiParticle);
+
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Creating particles for testing..." << std::endl;
+    }
+    //create particles for testing
+    int numberOfParticles = 10;
+    Particle particles[numberOfParticles];
+    initializeParticles(particles, numberOfParticles);
+
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Converting particle array to particle list..." << std::endl;
+    }
+    //convert array to particle list
+    ParticleList *particleList;
+    auto pList = new ParticleList;
+    particleList = pList;
+    createLocalParticleList(s, pList, particles, numberOfParticles);
+    int particlesPerProcess = numberOfParticles/s.numprocs;
+
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Converting back to an array..." << std::endl;
+    }
+    //convert back to array
+    Particle pArray[particlesPerProcess];
+    Particle pReceived[particlesPerProcess];
+
+    convertListToArray(s, particleList, pArray, particlesPerProcess);
+
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Sending and receiving particles..." << std::endl;
+    }
     int from = s.myrank;
     int to;
     if (from == 1) {
@@ -119,33 +147,24 @@ int main(int argc, char *argv[]) {
     MPI_Request req;
     MPI_Status stat;
 
-    /*
-    int receive;
-    int send = s.myrank + 1000;
-    MPI_Isend(&send, 1, MPI_INT, to, 17, MPI_COMM_WORLD, &req);
-    MPI_Recv(&receive, 1, MPI_INT, to, 17, MPI_COMM_WORLD, &stat);
-     */
-
-    //MPI_Send(pArray, particlesPerProcess, mpiParticle, to, 1, MPI_COMM_WORLD);
-    //MPI_Irecv(pReceived, particlesPerProcess, mpiParticle, from, 0, MPI_COMM_WORLD, &req);
-
     MPI_Isend(pArray, particlesPerProcess, mpiParticle, to, 17, MPI_COMM_WORLD, &req);
     MPI_Recv(pReceived, particlesPerProcess, mpiParticle, to, 17, MPI_COMM_WORLD, &stat);
 
     MPI_Wait(&req, &stat);
     //MPI_Request_free(&req);
 
-    /*if (s.myrank == outputRank) {
-        std::cout << "received: " << receive << std::endl;
-    }*/
 
-    if (s.myrank != outputRank) {
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Checking results..." << std::endl;
+    }
+    /*if (s.myrank != outputRank) {
         std::cout << "EXPECTED" << std::endl;
         std::cout << "rank: " << s.myrank << std::endl;
         for (int i=0; i < particlesPerProcess; i++) {
             std::cout << "\texpected particle " << i << ": " << "(" << pArray[i].x[0] << ", " << pArray[i].x[1] << ", " << pArray[i].x[2] << ")" << std::endl;
         }
-    }
+    }*/
     if (s.myrank == outputRank) {
         std::cout << "RECEIVED" << std::endl;
         std::cout << "rank: " << s.myrank << std::endl;
@@ -156,6 +175,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
+
+    /** -------------------------------------- */
+    if (s.myrank == outputRank) {
+        std::cout << "Finished..." << std::endl;
+    }
 
     MPI_Finalize();
 
